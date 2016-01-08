6c64b36e851098a521625e01671b00f404f8195d
BackStreetSoy <jordanedwardobey@gmail.com>
2016-01-08 11:11:11 -0600

    prepping for precompile

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module KowabungaForever
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif *.css *.scss)
     config.assets.compile = true
     config.serve_static_files  = true
   end
